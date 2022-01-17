5640b4696c71c0a1c68cf4f45312a1a0aea554d4
tez4 <joel.twann@bluewin.ch>
2022-01-17 15:51:04 +0100

    Created more loops to get better results

--- test_database.py
+++ test_database.py
@@ -107,156 +107,157 @@ kk = [100,200,400,800,1600,3200,6400,12500,25000,50000,100000]
 for post_number in [100,200,400,800,1600,3200,6400]:
     create_database(post_number)
 
-    random_post_ids = [random.randint(0,post_number) for i in range(5)]
+    for j in range(20):
+        random_post_ids = [random.randint(0,post_number) for i in range(5)]
 
-    # query one blogpost
-    for random_post_id in random_post_ids:
-        df_results, df_time = get_sql_time(
-            ["""
-            SELECT *
-            FROM public.posts p
-            LEFT JOIN public.comments c
-            ON (p.post_id = c.post_id)
-            LEFT JOIN public.tags t
-            ON (p.post_id = t.post_id)
-            WHERE p.post_id = %s
-            """],
-            [(random_post_id,)],
-            'one blogpost',
-            df_time,
-            post_number,
-            'SQL with joins'
-        )
+        # query one blogpost
+        for random_post_id in random_post_ids:
+            df_results, df_time = get_sql_time(
+                ["""
+                SELECT *
+                FROM public.posts p
+                LEFT JOIN public.comments c
+                ON (p.post_id = c.post_id)
+                LEFT JOIN public.tags t
+                ON (p.post_id = t.post_id)
+                WHERE p.post_id = %s
+                """],
+                [(random_post_id,)],
+                'one blogpost',
+                df_time,
+                post_number,
+                'SQL with joins'
+            )
 
-        df_results, df_time = get_sql_time(
-            ["""
-            SELECT *
-            FROM public.mv_one_blogpost
-            WHERE post_id = %s
-            """],
-            [(random_post_id,)],
-            'one blogpost',
-            df_time,
-            post_number,
-            'SQL Materialized View'
-        )
+            df_results, df_time = get_sql_time(
+                ["""
+                SELECT *
+                FROM public.mv_one_blogpost
+                WHERE post_id = %s
+                """],
+                [(random_post_id,)],
+                'one blogpost',
+                df_time,
+                post_number,
+                'SQL Materialized View'
+            )
+            
+            df_results, df_time = get_sql_time(
+                [
+                    """
+                    SELECT *
+                    FROM public.posts
+                    WHERE post_id = %s
+                    """,
+                    """
+                    SELECT *
+                    FROM public.comments
+                    WHERE post_id = %s
+                    """,
+                    """
+                    SELECT *
+                    FROM public.tags
+                    WHERE post_id = %s
+                    """
+                ],
+                [
+                    (random_post_id,),
+                    (random_post_id,),
+                    (random_post_id,)
+                ],
+                'one blogpost',
+                df_time,
+                post_number,
+                'SQL without joins'
+            )
+
+            cursor, df_time = get_mongodb_time('find',{'post_id' : random_post_id}, 'one blogpost', df_time, post_number)
+
+            # ids = []
+            # for doc in cursor:
+            #     ids.append(doc['post_id'])
+
+        # query all blogposts of often used tag
+        df_tags = run_sql_query(
+            """
+            SELECT COUNT(tag_id) count, tag
+            FROM public.tags
+            GROUP BY tag
+            ORDER BY count DESC, tag
+            LIMIT 5
+            """)
         
-        df_results, df_time = get_sql_time(
-            [
-                """
+        for tag in [tag for tag in df_tags['tag']]:
+            
+            df_results, df_time = get_sql_time(
+                ["""
                 SELECT *
-                FROM public.posts
-                WHERE post_id = %s
-                """,
-                """
+                FROM public.tags t
+                LEFT JOIN public.posts p
+                ON (t.post_id = p.post_id)
+                WHERE tag = %s
+                """],
+                [(tag,)],
+                'by tag',
+                df_time,
+                post_number,
+                'SQL'
+            )
+
+            df_results, df_time = get_sql_time(
+                ["""
                 SELECT *
-                FROM public.comments
-                WHERE post_id = %s
-                """,
-                """
-                SELECT *
-                FROM public.tags
-                WHERE post_id = %s
-                """
-            ],
-            [
-                (random_post_id,),
-                (random_post_id,),
-                (random_post_id,)
-            ],
-            'one blogpost',
-            df_time,
-            post_number,
-            'SQL without joins'
-        )
+                FROM public.mv_tags
+                WHERE tag = %s
+                """],
+                [(tag,)],
+                'by tag',
+                df_time,
+                post_number,
+                'SQL Materialized View'
+            )
 
-        cursor, df_time = get_mongodb_time('find',{'post_id' : random_post_id}, 'one blogpost', df_time, post_number)
-
-        # ids = []
-        # for doc in cursor:
-        #     ids.append(doc['post_id'])
-
-    # query all blogposts of often used tag
-    df_tags = run_sql_query(
-        """
-        SELECT COUNT(tag_id) count, tag
-        FROM public.tags
-        GROUP BY tag
-        ORDER BY count DESC, tag
-        LIMIT 5
-        """)
-    
-    for tag in [tag for tag in df_tags['tag']]:
+            cursor, df_time = get_mongodb_time('find',{'tags': {'$all': [tag]}}, 'by tag', df_time, post_number)
         
-        df_results, df_time = get_sql_time(
-            ["""
-            SELECT *
-            FROM public.tags t
-            LEFT JOIN public.posts p
-            ON (t.post_id = p.post_id)
-            WHERE tag = %s
-            """],
-            [(tag,)],
-            'by tag',
-            df_time,
-            post_number,
-            'SQL'
-        )
+        # query all comments of a certain user
+        df_comments = run_sql_query(
+            """
+            SELECT COUNT(comment_id) count, comment_author
+            FROM public.comments
+            GROUP BY comment_author
+            ORDER BY count DESC, comment_author
+            LIMIT 5
+            """)
+        
+        for author in [author for author in df_comments['comment_author']]: 
 
-        df_results, df_time = get_sql_time(
-            ["""
-            SELECT *
-            FROM public.mv_tags
-            WHERE tag = %s
-            """],
-            [(tag,)],
-            'by tag',
-            df_time,
-            post_number,
-            'SQL Materialized View'
-        )
+            df_results, df_time = get_sql_time(
+                ["""
+                SELECT *
+                FROM public.comments c
+                WHERE comment_author = %s
+                """],
+                [(author,)],
+                'by comment author',
+                df_time,
+                post_number,
+                'SQL'
+            )
 
-        cursor, df_time = get_mongodb_time('find',{'tags': {'$all': [tag]}}, 'by tag', df_time, post_number)
-    
-    # query all comments of a certain user
-    df_comments = run_sql_query(
-        """
-        SELECT COUNT(comment_id) count, comment_author
-        FROM public.comments
-        GROUP BY comment_author
-        ORDER BY count DESC, comment_author
-        LIMIT 5
-        """)
-    
-    for author in [author for author in df_comments['comment_author']]: 
+            df_results, df_time = get_sql_time(
+                ["""
+                SELECT *
+                FROM public.mv_comments
+                WHERE comment_author = %s
+                """],
+                [(author,)],
+                'by comment author',
+                df_time,
+                post_number,
+                'SQL with Index'
+            )
 
-        df_results, df_time = get_sql_time(
-            ["""
-            SELECT *
-            FROM public.comments c
-            WHERE comment_author = %s
-            """],
-            [(author,)],
-            'by comment author',
-            df_time,
-            post_number,
-            'SQL'
-        )
-
-        df_results, df_time = get_sql_time(
-            ["""
-            SELECT *
-            FROM public.mv_comments
-            WHERE comment_author = %s
-            """],
-            [(author,)],
-            'by comment author',
-            df_time,
-            post_number,
-            'SQL with Index'
-        )
-
-        cursor, df_time = get_mongodb_time('find',{'comments.comment_author': author}, 'by comment author', df_time, post_number)
+            cursor, df_time = get_mongodb_time('find',{'comments.comment_author': author}, 'by comment author', df_time, post_number)
 
 
 df_time.to_csv('output/time.csv', index=False)
